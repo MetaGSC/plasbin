cc23c079607ea00c87581eaee3200bcff6692bc0
chamikaCN <chamikanandasiri97@gmail.com>
2021-08-05 10:34:49 +0530

    removed for loops in import method

--- helper functions/HDF5datasetCreator.py
+++ helper functions/HDF5datasetCreator.py
@@ -4,24 +4,26 @@ import re
 import h5py
 import numpy as np
 
-def _import_gz_to_hdf5_datasets(gz_directory_path,hdf5_filepath):
+
+def _import_gz_to_hdf5_datasets(gz_directory_path, hdf5_filepath):
     sub_directory_array = os.listdir(gz_directory_path+'Data/')
     print(sub_directory_array)
     for filename in os.listdir(gz_directory_path+'Data/'+sub_directory_array[0]):
         if filename.endswith(".gz"):
             fileidentifier = re.sub('\.gz', '', filename)
-            for line in gzip.open(gz_directory_path+'Data/'+sub_directory_array[0]+'/'+filename,'rt'):
-                data_array = np.array(line.split(" ")).astype(np.float)
-                for j in range(1,len(sub_directory_array)):
-                    for sub_line in gzip.open(gz_directory_path+'Data/'+sub_directory_array[j]+'/'+filename,'rt'):
-                        sub_data_array = np.array(sub_line.split(" ")).astype(np.float)
-                        data_array = np.append(data_array,sub_data_array)                        
-                        break
-                for label_line in gzip.open(gz_directory_path+'Label/'+filename,'rt'):
-                    label_data_array = np.array(sub_line.split(" ")).astype(np.int)
-                    break
-                _write_to_h5(hdf5_filepath,data_array,label_data_array, fileidentifier)
-                break
+            with gzip.open(gz_directory_path+'Data/'+sub_directory_array[0]+'/'+filename, 'rt') as f:
+                data_array = np.array(
+                    f.readline().rstrip().split(" ")).astype(np.float)
+                for j in range(1, len(sub_directory_array)):
+                    with gzip.open(gz_directory_path+'Data/'+sub_directory_array[j]+'/'+filename, 'rt') as g:
+                        sub_data_array = np.array(
+                            g.readline().rstrip().split(" ")).astype(np.float)
+                        data_array = np.append(data_array, sub_data_array)
+                with gzip.open(gz_directory_path+'Label/'+filename, 'rt') as h:
+                    label_data_array = np.array(
+                        h.readline().rstrip().split(" ")).astype(np.float).astype(np.int)
+                _write_to_h5(hdf5_filepath, data_array,
+                             label_data_array, fileidentifier)
         else:
             continue
 			
